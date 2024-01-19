7830d9c8a1524fda153b8630dab7a5abf320d5ee
prerna3145 <115696276+prerna3145@users.noreply.github.com>
2024-01-19 19:04:25 +0530

    summary overall cost material,category spend views

--- pep-potato-sourcing-matrix-automation/src/summary_overall_cost.py
+++ pep-potato-sourcing-matrix-automation/src/summary_overall_cost.py
@@ -1,7 +1,9 @@
 """Summary Overall Cost  API"""
 from database import get_db
 from fastapi import APIRouter, Depends, HTTPException
-from models import summary_overall_cost,summary_total_exp_wo_solids,summary_total_exp_w_solids
+from models import (summary_overall_cost,summary_total_exp_wo_solids
+,summary_total_exp_w_solids,summary_material_spend,summary_freight_spend,
+summary_overall_cost_solids,summary_category_spend)
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -39,5 +41,53 @@ def summary_total_exp_w_solids_year_country_code(year: int, company_name: str, d
             summary_total_exp_w_solids.columns.company_name == company_name
             ).order_by(summary_total_exp_w_solids.columns.period).all()
         return {"summary_total_exp_w_solids": records}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+@router.get('overall_cost_material_spend/year/{year}/company_name/{company_name}')
+def summary_material_spend_year_country_code(year: int, company_name: str, db: Session = Depends(get_db)): # pragma: no cover
+    """Function to fetch all records from summary_material_spend view based on year and country_Code filter """
+    try:
+        records = db.query(summary_material_spend).filter(
+            summary_material_spend.columns.year == year,
+            summary_material_spend.columns.company_name == company_name
+            ).order_by(summary_material_spend.columns.period).all()
+        return {"summary_material_spend": records}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+    
+@router.get('overall_cost_freight_spend/year/{year}/company_name/{company_name}')
+def summary_freight_spend_year_country_code(year: int, company_name: str, db: Session = Depends(get_db)): # pragma: no cover
+    """Function to fetch all records from summary_freight_spend view based on year and country_Code filter """
+    try:
+        records = db.query(summary_freight_spend).filter(
+            summary_freight_spend.columns.year == year,
+            summary_freight_spend.columns.company_name == company_name
+            ).order_by(summary_freight_spend.columns.period).all()
+        return {"summary_freight_spend": records}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+@router.get('overall_cost_solids/year/{year}/company_name/{company_name}')
+def summary_overall_cost_solids_year_country_code(year: int, company_name: str, db: Session = Depends(get_db)): # pragma: no cover
+    """Function to fetch all records from summary_overall_cost_solids view based on year and country_Code filter """
+    try:
+        records = db.query(summary_overall_cost_solids).filter(
+            summary_overall_cost_solids.columns.year == year,
+            summary_overall_cost_solids.columns.company_name == company_name
+            ).order_by(summary_overall_cost_solids.columns.period).all()
+        return {"summary_overall_cost_solids": records}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+@router.get('overall_cost_category_spend/year/{year}/company_name/{company_name}')
+def summary_category_spend_year_country_code(year: int, company_name: str, db: Session = Depends(get_db)): # pragma: no cover
+    """Function to fetch all records from summary_overall_cost_solids view based on year and country_Code filter """
+    try:
+        records = db.query(summary_category_spend).filter(
+            summary_category_spend.columns.year == year,
+            summary_category_spend.columns.company_name == company_name
+            ).order_by(summary_category_spend.columns.period).all()
+        return {"summary_category_spend": records}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e)) from e
