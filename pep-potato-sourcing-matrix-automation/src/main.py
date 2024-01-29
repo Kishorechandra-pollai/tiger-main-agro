7e205626a79e6c35b7cffcc437edc6547ec765aa
Anirudh K V <anirudh.vadakkep@tigeranalytics.com>
2024-01-29 07:48:16 +0530

    added router for infl_defl_py

--- pep-potato-sourcing-matrix-automation/src/main.py
+++ pep-potato-sourcing-matrix-automation/src/main.py
@@ -31,6 +31,8 @@ import inflation_deflation
 import dashboard
 import summary_solids
 import summary_overall_cost
+import plant_mtrx_templt
+import infl_defl_py
 from database import engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -76,6 +78,8 @@ app.include_router(summary_solids.router, tags=['summary_solids'], prefix='/api/
 app.include_router(summary_overall_cost.router, tags=['summary_overall_cost'], prefix='/api/summary_overall_cost')
 
 app.include_router(plant_mtrx_templt.router, tags=['plant_mtrx_templt'], prefix='/api/plant_mtrx_templt')
+
+app.include_router(infl_defl_py.router, tags=['infl_defl_py'], prefix='/api/infl_defl_py')
 
 app.add_middleware(
     CORSMiddleware,
