227fe9eaaea23434117c647438436feb78c0c099
Anirudh K V <anirudh.vadakkep@tigeranalytics.com>
2024-02-15 13:24:00 +0530

    updated code to handle more combination in add new plant

--- pep-potato-sourcing-matrix-automation/src/masters_mapping.py
+++ pep-potato-sourcing-matrix-automation/src/masters_mapping.py
@@ -7,7 +7,7 @@ import models
 import schemas
 from potatorates import create_potato_rate_in_db, update_potato_rates_default
 from solidrates import create_solid_rate_in_db, update_solids_rates_default
-from freightcost import create_freight_rates_in_db,update_freight_cost_mapping_with_default_value
+from freightcost import create_freight_rates_in_db,update_freight_cost_mapping_with_default_value,update_freight_rates_with_default_value
 
 router = APIRouter()
 
@@ -27,6 +27,67 @@ def get_plantMtrx_by_region(db: Session = Depends(get_db)): # pragma: no cover
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+def new_ga_potato_solids(db,payload,current_time):
+    growing_area_id = db.query(func.max(models.growing_area.growing_area_id)).scalar() or 0
+    growing_area_id += 1
+    ga_payload = {
+        "growing_area_name": payload.psga_map.growing_area, 
+        "country": payload.growing_area.country,
+        "created_by": "JP",
+        "created_time": current_time,
+        "updated_by": "JP",
+        "updated_time": current_time,
+        "status": payload.growing_area.ga_status,
+        "region": payload.plant.region_id, 
+        "growing_area_desc": payload.growing_area.ga_desc,
+        "fresh_period_start": payload.growing_area.fp_start,
+        "fresh_period_end": payload.growing_area.fp_end,
+        "fresh_week_start": payload.growing_area.fw_start,
+        "fresh_week_end": payload.growing_area.fw_end,
+        "storage_period_start": payload.growing_area.sp_start,
+        "storage_week_start": payload.growing_area.sw_start,
+        "growing_area_id": growing_area_id
+        }
+
+    # Insert into growing_area table
+    new_growing_area = models.growing_area(**ga_payload)
+    db.add(new_growing_area)
+    print("inserted into growing area table")
+
+    potato_rate_id = db.query(func.max(models.potato_rates.potato_rate_id)).scalar() or 0
+    potato_rate_id += 1
+    potato_rate_payload = {"potato_rate_id":potato_rate_id,
+                            "year":datetime.utcnow().year,
+                            "growing_area_id_old":999,
+                            "created_time":current_time,
+                            "created_by":"System",
+                            "updated_time":current_time,
+                            "updated_by":"SYSTEM",
+                            "currency":"USD",
+                            "growing_area_id":growing_area_id}
+    create_potato_rate_in_db(potato_rate_payload, db)
+
+    ## Solids Ratese
+    solids_rate_id = db.query(func.max(models.solids_rates.solids_rate_id)).scalar() or 0
+    solids_rate_id += 1
+    solids_rate_payload = {"solids_rate_id":solids_rate_id,
+                            "growing_area_id_old":999,
+                            "created_time":current_time,
+                            "created_by":"System",
+                            "updated_time":current_time,
+                            "updated_by":"SYSTEM",
+                            "currency":"USD",
+                            "growing_area_id":growing_area_id}
+    create_solid_rate_in_db(solids_rate_payload, db)
+
+    ## Potato mappings
+    update_potato_rates_default(db,potato_rate_id,datetime.utcnow().year)
+
+    ## Solids mappings
+    update_solids_rates_default(db, solids_rate_id, datetime.utcnow().year)
+    print("inserted into potato,solid rate and mapping table")
+    return new_growing_area,True
+
 
 @router.post('/add_plant_mapping', status_code=status.HTTP_201_CREATED)
 def create_plant(payload: schemas.MastersMapping, db: Session = Depends(get_db)): # pragma: no cover
@@ -37,8 +98,11 @@ def create_plant(payload: schemas.MastersMapping, db: Session = Depends(get_db))
         if plant_count>0:
             return {"status":"plant already exists"}
         ga_vsc = False
+        ga_status = False
         vendor_site_code = payload.psga_map.Vendor_Site_Code
         count = db.query(models.vendor_site_code).filter(models.vendor_site_code.VENDOR_SITE_CODE == vendor_site_code).count()
+        growing_area_name = payload.psga_map.growing_area
+        count_ga = db.query(models.growing_area).filter(models.growing_area.growing_area_name == growing_area_name).count()
 
         current_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]  # Or use the appropriate timezone
 
@@ -57,9 +121,25 @@ def create_plant(payload: schemas.MastersMapping, db: Session = Depends(get_db))
         
         row_id = db.query(func.max(models.PlantSiteGrowingAreaMapping.row_id)).scalar() or 0
         row_id += 1
+        print("inserted into plant table")
 
-        if count>0:
-            growing_area_name = payload.psga_map.growing_area
+        if count>0: # Existing vendor site
+            if count_ga>0:
+                ## existing growing area ; only need to add in psga
+                growing_area_id = db.query(models.growing_area.growing_area_id).filter(
+                models.growing_area.growing_area_name == growing_area_name).first()[0]
+                print("existing growing area and existing vendor site")
+            else:
+                # if new growing area
+                new_growing_area,ga_status = new_ga_potato_solids(db,payload,current_time)
+                print("new growing area and existing vendor site")
+                growing_area_id = db.query(func.max(models.PlantSiteGrowingAreaMapping.growing_area_id)).scalar() or 0
+                growing_area_id += 1
+            ## If new growing area or not should be checked here
+            ## If new growing area, increment id by one,  potato and solid rate tables would be used only if it is new
+                
+            # growing_area_name = payload.psga_map.growing_area
+                
             # Fetching the vendor site ID
             # Fetch the vendor site ID directly
             vendor_site_id = db.query(models.vendor_site_code.VENDOR_SITE_ID).filter(
@@ -67,58 +147,69 @@ def create_plant(payload: schemas.MastersMapping, db: Session = Depends(get_db))
             ).first()[0]
 
             # Fetch the growing area ID directly
-            growing_area_id = db.query(models.PlantSiteGrowingAreaMapping.growing_area_id).filter(
-                models.PlantSiteGrowingAreaMapping.Vendor_Site_Code == vendor_site_code,
-                models.PlantSiteGrowingAreaMapping.growing_area == growing_area_name
-            ).first()[0]
+            # growing_area_id = db.query(models.PlantSiteGrowingAreaMapping.growing_area_id).filter(
+            #     models.PlantSiteGrowingAreaMapping.Vendor_Site_Code == vendor_site_code,
+            #     models.PlantSiteGrowingAreaMapping.growing_area == growing_area_name
+            # ).first()[0]
 
 
             ## remove vendor_site_id and growing_area_id from payload
-            new_mapping = models.PlantSiteGrowingAreaMapping(
-                **payload.psga_map.dict(),
-                row_id=row_id,
-                plant_id=plant_id,
-                vendor_site_id=vendor_site_id,
-                growing_area_id=growing_area_id
-            )
-            db.add(new_mapping)
-        else:
-            growing_area_id = db.query(func.max(models.PlantSiteGrowingAreaMapping.growing_area_id)).scalar() or 0
-            growing_area_id += 1
+            # new_mapping = models.PlantSiteGrowingAreaMapping(
+            #     **payload.psga_map.dict(),
+            #     row_id=row_id,
+            #     plant_id=plant_id,
+            #     vendor_site_id=vendor_site_id,
+            #     growing_area_id=growing_area_id
+            # )
+            # db.add(new_mapping)
+        else: ## New vendor site
+            ## Is it a new growing area or existing growing area should be shown here
+            ## If new growing area, increment id by one,  potato and solid rate tables would be used only if it is new
+            if count_ga>0:
+                ## existing growing area ; only need to add in psga
+                growing_area_id = db.query(models.growing_area.growing_area_id).filter(
+                models.growing_area.growing_area_name == growing_area_name).first()[0]
+                print("existing growing area and new vendor site")
+            else: # if new growing area
+                new_growing_area,ga_status = new_ga_potato_solids(db,payload,current_time)
+                print("new growing area and new vendor site")
+                growing_area_id = db.query(func.max(models.PlantSiteGrowingAreaMapping.growing_area_id)).scalar() or 0
+                growing_area_id += 1
+                
             vendor_site_id = db.query(func.max(models.vendor_site_code.VENDOR_SITE_ID)).scalar() or 0
             vendor_site_id += 1
 
-            new_mapping = models.PlantSiteGrowingAreaMapping(
-                **payload.psga_map.dict(),
-                row_id=row_id,
-                plant_id=plant_id,
-                vendor_site_id=vendor_site_id,
-                growing_area_id=growing_area_id
-            )
-            db.add(new_mapping)
+            # new_mapping = models.PlantSiteGrowingAreaMapping(
+            #     **payload.psga_map.dict(),
+            #     row_id=row_id,
+            #     plant_id=plant_id,
+            #     vendor_site_id=vendor_site_id,
+            #     growing_area_id=growing_area_id
+            # )
+            # db.add(new_mapping)
 
-            ga_payload = {
-                "growing_area_name": payload.psga_map.growing_area, 
-                "country": payload.growing_area.country,
-                "created_by": "JP",
-                "created_time": current_time,
-                "updated_by": "JP",
-                "updated_time": current_time,
-                "status": payload.growing_area.ga_status,
-                "region": payload.plant.region_id, 
-                "growing_area_desc": payload.growing_area.ga_desc,
-                "fresh_period_start": payload.growing_area.fp_start,
-                "fresh_period_end": payload.growing_area.fp_end,
-                "fresh_week_start": payload.growing_area.fw_start,
-                "fresh_week_end": payload.growing_area.fw_end,
-                "storage_period_start": payload.growing_area.sp_start,
-                "storage_week_start": payload.growing_area.sw_start,
-                "growing_area_id": growing_area_id
-            }
+            # ga_payload = {
+            #     "growing_area_name": payload.psga_map.growing_area, 
+            #     "country": payload.growing_area.country,
+            #     "created_by": "JP",
+            #     "created_time": current_time,
+            #     "updated_by": "JP",
+            #     "updated_time": current_time,
+            #     "status": payload.growing_area.ga_status,
+            #     "region": payload.plant.region_id, 
+            #     "growing_area_desc": payload.growing_area.ga_desc,
+            #     "fresh_period_start": payload.growing_area.fp_start,
+            #     "fresh_period_end": payload.growing_area.fp_end,
+            #     "fresh_week_start": payload.growing_area.fw_start,
+            #     "fresh_week_end": payload.growing_area.fw_end,
+            #     "storage_period_start": payload.growing_area.sp_start,
+            #     "storage_week_start": payload.growing_area.sw_start,
+            #     "growing_area_id": growing_area_id
+            # }
 
-            # Insert into growing_area table
-            new_growing_area = models.growing_area(**ga_payload)
-            db.add(new_growing_area)
+            # # Insert into growing_area table
+            # new_growing_area = models.growing_area(**ga_payload)
+            # db.add(new_growing_area)
 
             vsc_payload = {
                 "VENDOR_SITE_ID":vendor_site_id,
@@ -134,41 +225,53 @@ def create_plant(payload: schemas.MastersMapping, db: Session = Depends(get_db))
             new_vendor_site_code = models.vendor_site_code(**vsc_payload)
             db.add(new_vendor_site_code)
             ga_vsc=True
+            print("inserted into vendor site table")
 
 
             # Potato Rates
-            potato_rate_id = db.query(func.max(models.potato_rates.potato_rate_id)).scalar() or 0
-            potato_rate_id += 1
-            potato_rate_payload = {"potato_rate_id":potato_rate_id,
-                                    "year":datetime.utcnow().year,
-                                    "growing_area_id_old":999,
-                                    "created_time":current_time,
-                                    "created_by":"System",
-                                    "updated_time":current_time,
-                                    "updated_by":"SYSTEM",
-                                    "currency":"USD",
-                                    "growing_area_id":growing_area_id}
-            create_potato_rate_in_db(potato_rate_payload, db)
+            # potato_rate_id = db.query(func.max(models.potato_rates.potato_rate_id)).scalar() or 0
+            # potato_rate_id += 1
+            # potato_rate_payload = {"potato_rate_id":potato_rate_id,
+            #                         "year":datetime.utcnow().year,
+            #                         "growing_area_id_old":999,
+            #                         "created_time":current_time,
+            #                         "created_by":"System",
+            #                         "updated_time":current_time,
+            #                         "updated_by":"SYSTEM",
+            #                         "currency":"USD",
+            #                         "growing_area_id":growing_area_id}
+            # create_potato_rate_in_db(potato_rate_payload, db)
 
-            ## Solids Ratese
-            solids_rate_id = db.query(func.max(models.solids_rates.solids_rate_id)).scalar() or 0
-            solids_rate_id += 1
-            solids_rate_payload = {"solids_rate_id":solids_rate_id,
-                                    "growing_area_id_old":999,
-                                    "created_time":current_time,
-                                    "created_by":"System",
-                                    "updated_time":current_time,
-                                    "updated_by":"SYSTEM",
-                                    "currency":"USD",
-                                    "growing_area_id":growing_area_id}
-            create_solid_rate_in_db(solids_rate_payload, db)
+            # ## Solids Ratese
+            # solids_rate_id = db.query(func.max(models.solids_rates.solids_rate_id)).scalar() or 0
+            # solids_rate_id += 1
+            # solids_rate_payload = {"solids_rate_id":solids_rate_id,
+            #                         "growing_area_id_old":999,
+            #                         "created_time":current_time,
+            #                         "created_by":"System",
+            #                         "updated_time":current_time,
+            #                         "updated_by":"SYSTEM",
+            #                         "currency":"USD",
+            #                         "growing_area_id":growing_area_id}
+            # create_solid_rate_in_db(solids_rate_payload, db)
 
-            ## Potato mappings
-            update_potato_rates_default(db,potato_rate_id,datetime.utcnow().year)
+            # ## Potato mappings
+            # update_potato_rates_default(db,potato_rate_id,datetime.utcnow().year)
 
-            ## Solids mappings
-            update_solids_rates_default(db, solids_rate_id, datetime.utcnow().year)
+            # ## Solids mappings
+            # update_solids_rates_default(db, solids_rate_id, datetime.utcnow().year)
+
+        ## Add in PSGA mapping table
+        new_mapping = models.PlantSiteGrowingAreaMapping(
+            **payload.psga_map.dict(),
+            row_id=row_id,
+            plant_id=plant_id,
+            vendor_site_id=vendor_site_id,
+            growing_area_id=growing_area_id)
+        db.add(new_mapping)
+        print("inserted into psga map table")
         
+        ## Freight cost should be handled in edit plant as well (possiblity of new GA Or VSC)
         freight_cost_id = db.query(func.max(models.FreightCostRate.freight_cost_id)).scalar() or 0
         freight_cost_id += 1
         freight_cost_rate_payload = {"freight_cost_id":freight_cost_id,
@@ -184,14 +287,19 @@ def create_plant(payload: schemas.MastersMapping, db: Session = Depends(get_db))
         create_freight_rates_in_db(freight_cost_rate_payload, db)
 
         ## Freight Cost mappings
-        update_freight_cost_mapping_with_default_value(freight_cost_id,datetime.utcnow().year,db)   
+        # update_freight_cost_mapping_with_default_value(freight_cost_id,datetime.utcnow().year,db) 
+        update_freight_rates_with_default_value(freight_cost_id,datetime.utcnow().year,db)  
+        print("inserted into freight rate and mapping table")
 
         db.commit()
         db.refresh(new_plant)
         db.refresh(new_mapping)
         if ga_vsc:
-            db.refresh(new_growing_area)
+            # db.refresh(new_growing_area)
             db.refresh(new_vendor_site_code)
+        ## need to add refresh of growing area here in another condition
+        if ga_status:
+            db.refresh(new_growing_area)
         return {"status": "New plant added successfully"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
