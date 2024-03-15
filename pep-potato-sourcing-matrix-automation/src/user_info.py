1434ba231562179cdfa37587f3733ada9ac06a48
prerna3145 <115696276+prerna3145@users.noreply.github.com>
2024-03-15 15:35:28 +0530

    updated response for user page mapping view

--- pep-potato-sourcing-matrix-automation/src/user_info.py
+++ pep-potato-sourcing-matrix-automation/src/user_info.py
@@ -35,12 +35,44 @@ def create_user_information(payload: schemas.UserInfoSchema, db: Session = Depen
     db.refresh(user_info)
     return {"status": "success"}
 
+# @router.get('/user_page_mapping_view')
+# def user_page_mapping_view(db: Session = Depends(get_db)):
+#     """Function to fetch all records from user_information view table """
+#     try:
+#         records = db.query(user_page_mapping_table).all()
+#         return {"message": "Success", "data": records}
+#     except Exception as e:
+#         error_detail = str(e)
+#         raise HTTPException(status_code=400, detail=error_detail) from e
 @router.get('/user_page_mapping_view')
 def user_page_mapping_view(db: Session = Depends(get_db)):
-    """Function to fetch all records from user_information view table """
+    """Function to fetch records from user_information view table """
     try:
         records = db.query(user_page_mapping_table).all()
-        return {"message": "Success", "data": records}
+        
+        response_data = []
+        for record in records:
+            country_name = "US,Canada" if record.country_name == "All" else record.country_name
+            response_data = [
+            {
+                "user_id": record.user_id,  
+                "page_id": record.page_id, 
+                "country_id": record.country_id,  
+                "access_id": record.access_id, 
+                "email": record.email,  
+                "country": record.country, 
+                "first_name": record.first_name,  
+                "last_name": record.last_name, 
+                "is_admin": record.is_admin,  
+                "user_status": record.user_status, 
+                "page_name": record.page_name,  
+                "access_name": record.access_name,
+                "access_value": record.access_value,
+                "country_name": country_name
+            }
+            for record in records
+        ]
+        return {"message": "Success", "data": response_data}
     except Exception as e:
         error_detail = str(e)
         raise HTTPException(status_code=400, detail=error_detail) from e
