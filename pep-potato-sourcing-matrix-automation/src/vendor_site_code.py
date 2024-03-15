079aefc26f9a05916f3bae4c02f458b5e06119f6
Anirudh K V <anirudh.vadakkep@tigeranalytics.com>
2024-03-15 14:19:23 +0530

    fetch active vendor sitees

--- pep-potato-sourcing-matrix-automation/src/vendor_site_code.py
+++ pep-potato-sourcing-matrix-automation/src/vendor_site_code.py
@@ -1,6 +1,6 @@
 """Vendor_Site_Code API"""
 from database import get_db
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from models import vendor_site_code
 from sqlalchemy.orm import Session
 
@@ -11,7 +11,10 @@ router = APIRouter()
 def view_vendor_site_code(db: Session = Depends(get_db)):
     """Function to fetch all records from vendor_site_code table """
     try:
-        records = db.query(vendor_site_code).all()
+        records = db.query(vendor_site_code).filter(vendor_site_code.status == "ACTIVE").all()
+        if not records:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"No vendor site code found")
         return {"data": records}
     except Exception as e: # pragma: no cover
         raise HTTPException(status_code=400, detail=str(e))
